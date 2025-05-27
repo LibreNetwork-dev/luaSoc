b7f41ff85f6042f2baed54764d5b4492a32fc7ee
Shrey719 <noreply@github.com>
2025-05-26 21:28:50 -0400

    things

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,25 @@
-use mlua::{Lua, Result, Function, Table, Value, lua_module};
+use mlua::{Lua, Result, Table, lua_module};
 use std::os::unix::net::UnixStream;
-use std::io::Write;
+use std::io::{Write, Read};
 
 #[lua_module]
 fn luaSoc(lua: &Lua) -> Result<Table> {
     let exports = lua.create_table()?;
 
-    let sendData = lua.create_function(|_, (path, data): (String, String)| {
-        let mut stream = UnixStream::connect(path)
-            .map_err(|e| mlua::Error::external(format!("Failed to connect socket: {}", e)))?;
+    let send_data = lua.create_function(|_, (path, data): (String, String)| {
+        let mut stream = UnixStream::connect(&path).map_err(|e| mlua::Error::external(format!("Failed to connect to socket: {}", e)))?;
 
-        stream.write_all(data.as_bytes())
-            .map_err(|e| mlua::Error::external(format!("Failed to write data: {}", e)))?;
+        stream.write_all(data.as_bytes()).map_err(|e| mlua::Error::external(format!("Failed to write: {}", e)))?;
 
-        Ok(Value::Nil)
+        stream.flush().map_err(|e| mlua::Error::external(format!("Failed to flush: {}", e)))?;
+
+        let mut res = Vec::new();
+        stream.read_to_end(&mut res).map_err(|e| mlua::Error::external(format!("Failed to read response: {}", e)))?;
+
+        let res_s = String::from_utf8_lossy(&res).to_string();
+        Ok(res_s) 
     })?;
 
-    exports.set("sendData", sendData)?;
-
+    exports.set("sendData", send_data)?;
     Ok(exports)
 }
